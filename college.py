4207920b17af5f0b1274eefe1a5b6cb2820d1679
Devsixth <129255859+Devsixth@users.noreply.github.com>
2023-04-04 13:58:41 +0530

    Update college.py

--- college.py
+++ college.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import seaborn as sns
 import plotly.express as px
 from PIL import Image
 from streamlit_option_menu import option_menu
